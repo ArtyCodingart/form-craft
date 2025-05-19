134abcf77cea60889c24780cc4deac4a375b62a1
Demetri0 <d3i0@ya.ru>
2025-05-19 23:44:46 +0500

    Experiment: Add tests to CI

--- Tests/FormCraftTests/FormCraftTests.swift
+++ Tests/FormCraftTests/FormCraftTests.swift
@@ -1,6 +1,12 @@
 import Testing
 @testable import FormCraft
 
-@Test func example() async throws {
+@Test func exampleBoolTest() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    let validation = FormCraftBooleanValidation()
+
+    // true → should pass
+    #expect(validation.validate(value: true), because: "По умолчанию значение true должно считаться валидным")
+    // false → should fails
+    #expect(!validation.validate(value: false), because: "false не равняется ожидаемому true")
 }
