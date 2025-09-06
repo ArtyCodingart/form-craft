7595c79d4608dea0c7f315c463ae2c490b56ae31
Demetri0 <d3i0@ya.ru>
2025-09-06 22:21:58 +0500

    chore: Make int.gt test as table

--- Tests/FormCraftTests/FormCraftIntegerValidationTests.swift
+++ Tests/FormCraftTests/FormCraftIntegerValidationTests.swift
@@ -3,20 +3,19 @@ import Testing
 
 struct FormCraftIntegerValidationTests {
     @Test
-    func testGtSucceedsForTrue() async {
-        let VAL = 10
-        let validation = FormCraftIntegerValidation().gt(num: 5)
-        let response = await validation.validate(value: VAL)
-        // Should be .success(true)
-        #expect({ if case .success(let v) = response { return v == VAL } else { return false } }())
-    }
+    func testGtValidation() async {
+        let validation = FormCraftIntegerValidation().gt(num: 5, message: "ERR")
 
-    @Test
-    func testGtFailsForFalse() async {
-        let ERR_MSG = "Should be greater than 5"
-        let validation = FormCraftIntegerValidation().gt(num: 5, message: ERR_MSG)
-        let response = await validation.validate(value: 2)
-        // Should be .error("Required")
-        #expect({ if case .error(let msg) = response { return msg == ERR_MSG } else { return false } }())
+        let cases: [(Int, FormCraftValidationResponse<Int>)] = [
+            (10, .success(value: 10)),
+            (6,  .success(value: 6)),
+            (5,  .error(message: "ERR")),
+            (2,  .error(message: "ERR")),
+        ]
+
+        for (input, expected) in cases {
+            let response = await validation.validate(value: input)
+            #expect(response == expected)
+        }
     }
 }
